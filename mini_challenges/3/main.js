cd12a7294136b7e785b6585e4ed8ced5b327b2dc
Meg Ducharme <ducharme.meg@gmail.com>
2018-05-07 11:04:07 -0500

    added comments

--- mini_challenges/3/main.js
+++ mini_challenges/3/main.js
@@ -4,6 +4,4 @@
 //2. When the button with id "btn-one-painting" is clicked, animate the image with id "painting-one" so that the opacity becomes 0.5 and the width increases by 100px.
 
 
-
-
 //3. When the button with id "btn-two-painting" is clicked, animate the image with id "painting-two" so that the image fades out over 1000 milliseconds and then fades in at a "slow" speed.
